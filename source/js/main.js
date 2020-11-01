f4e57e47c9b0d66acdc85dbfca6c0f33dae01b69
anddinara <and.dinara.moscow@gmail.com>
2020-11-01 22:17:10 +0300

    fix footer blocks

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,35 @@
-const anchors = document.querySelectorAll('a[href*="#"]')
+ 'use strict';
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+// const anchors = document.querySelectorAll('a[href*="#"]');
 
-    const blockID = anchor.getAttribute('href').substr(1)
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault()
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
+//     const blockID = anchor.getAttribute('href').substr(1);
+
+//     document.getElementById(blockID).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//   })
+// }
+
+// (function () {
+//   var navMain = document.querySelector('.main-nav__list');
+//   var navButton = document.querySelector('.main-nav__button');
+
+//   navMain.classList.remove('nav--nojs');
+
+//   var openNav = function () {
+//     if (navMain.classList.contains('nav--closed')) {
+//           navMain.classList.remove('nav--closed');
+//           navMain.classList.add('nav--opened');
+//     } else {
+//       navMain.classList.add('nav--closed');
+//       navMain.classList.remove('nav--opened');
+//     }
+//   };
+
+//   navToggle.addEventListener('click', openNav);
+// })();
